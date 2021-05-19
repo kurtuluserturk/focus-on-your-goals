54c27c9d68061bf34820cd5ffd5baa8e5d358156
Kurtuluş ERTÜRK <47227072+kurtuluserturk@users.noreply.github.com>
2021-05-19 22:45:29 +0300

    App wrapped with Provier and Router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { UserProvider } from './context'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <UserProvider>
+      <Router>
+        <App />
+      </Router>
+    </UserProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
